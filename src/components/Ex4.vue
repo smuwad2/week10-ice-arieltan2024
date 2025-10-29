6a0d05388db9a7138d5880981828df89cf2c5229
arieltan2024 <ariel.tan.2024@smu.edu.sg>
2025-10-29 03:39:12 +0000

    done in class w11

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,4 +1,5 @@
 <script>
+import TaskTracker from './subcomponents/TaskTracker.vue';
     export default {
         data() {
             return {
@@ -14,7 +15,13 @@
                 this.deadline = ''
             },
             // TODO: Add a new method, to delete a task completed
+            deleteTask(idx){
+                this.taskList.splice(idx, 1)
+            }
             
+        },
+        components:{
+            TaskTracker
         }
     }
 
@@ -34,7 +41,8 @@
     <hr>
 
     <!-- TODO: Modify following code -->
-    <task-tracker ></task-tracker>
+    <task-tracker v-for="(task,idx) in taskList"
+    :task="task" :idx="idx" :key="idx" @remove-task="deleteTask"></task-tracker>
 
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import router from './route/routes.js' // for routing 
 import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import 'bootstrap/dist/js/bootstrap.min.js'
 import App from './App.vue'
 
 
